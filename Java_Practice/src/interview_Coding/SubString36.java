3d269734d9a2bd380750d691f77ce20120b27f41
VIDYASAGAR.SHERI <VIDYASAGAR.SHERI@SHERIS-LAPTOP>
2025-11-17 22:00:16 +0530

    17th November 2025

--- Java_Practice/src/interview_Coding/SubString36.java
+++ Java_Practice/src/interview_Coding/SubString36.java
@@ -1,11 +1,14 @@
 package interview_Coding;
 import java.util.*;
 public class SubString36{
-
+// space is also one character
 	public static void main(String args[]) {
-		String str="I love automation testing";
+		/*String str="I love automation testing";
 
 		String str1=str.substring(18,25);
-		System.out.println(str1);
+		System.out.println(str1);*/
+		String str="I love my india";
+		String words[]=str.split(" ");
+		System.out.println(words[3]);
 	}
 }
