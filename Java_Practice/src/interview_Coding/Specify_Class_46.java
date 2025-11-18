fd8b8c882ec9f57f57464ae9796da6d1aa6ed88d
VIDYASAGAR.SHERI <VIDYASAGAR.SHERI@SHERIS-LAPTOP>
2025-11-18 11:32:57 +0530

    Pushed on 18/11/2025

--- Java_Practice/src/interview_Coding/Specify_Class_46.java
+++ Java_Practice/src/interview_Coding/Specify_Class_46.java
@@ -6,6 +6,8 @@ public class Specify_Class_46 {
         String word = "specify";
 
         // Loop through the string and print substrings
+        //The program prints all suffixes of the word “specify”.
+       // Each new line starts one character later in the word.
         for (int i = 0; i < word.length(); i++) {
             System.out.println(word.substring(i));
         }
