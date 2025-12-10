7ea2cc2debe92b2d8316ceab0a93de90a7abf2f2
VIDYASAGAR.SHERI <VIDYASAGAR.SHERI@SHERIS-LAPTOP>
2025-12-10 13:29:06 +0530

    10/12/2025

---
+++ Java_Practice/src/interview_Coding/NearestTO100Num_56.java
@@ -0,0 +1,21 @@
+package interview_Coding;
+
+public class NearestTO100Num_56 {
+
+	public static void main(String[] args) {
+		
+		 int a[] = {12, 5, 87, 32, 15, 98, 105, 124};
+	        int nearest = a[0], target = 100;      //Initially, we assume the first element (12) is the nearest.
+	        
+	        	//Math.abs(n - target) → difference between current number n and 100.
+	        	//Math.abs(nearest - target) → difference between nearest number so far and 100.
+	              for (int n : a)                                          //n takes each element of the array one by one.
+	              {
+	            if (Math.abs(n - target) < Math.abs(nearest - target))
+	            {
+	                nearest = n;                                       //If the current number n is closer to 100, then we update nearest.      
+	            }
+	              }
+	        System.out.println("Nearest number to 100 is: " + nearest);
+	}
+}
