5323de3c9e19d3684232e9fbf3a9d8e5ab03fae8
VIDYASAGAR.SHERI <VIDYASAGAR.SHERI@SHERIS-LAPTOP>
2025-12-13 14:00:33 +0530

    13/12/2025

---
+++ Java_Practice/src/interview_Coding/OtpFetchingFrmStrng_57.java
@@ -0,0 +1,24 @@
+package interview_Coding;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class OtpFetchingFrmStrng_57 {
+
+	public static void main(String[] args)
+	{
+		//Pattern is a class in Java used to define a regular expression.
+		//Matcher is a class in Java used to apply the Pattern on a given string.
+		//Pattern defines the OTP format, Matcher scans the string, find() locates the match, and group() returns the extracted OTP.
+		
+		String str = "hello masti 143vijii56 and graduate from bits765";
+		//One or more digits (143)  , One or more letters (vijii) , One or more digits (56)  , So it matches:::numbers + letters + numbers
+        Pattern p = Pattern.compile("\\d+[a-zA-Z]+\\d+");       //This line defines what OTP looks like.
+        Matcher m = p.matcher(str);                                                //Create Matcher Object, Matcher scans the string
+
+        if (m.find()) {                                                                               //checks whether the pattern exists  //Search this string using this pattern
+            System.out.println("OTP is: " + m.group());               //group() returns the matched text
+        }
+	}
+
+}
